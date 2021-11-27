5010e5468ba1f84153c11a79893d0114a0427959
patruggeri <patricio.ruggeri@gmail.com>
2021-11-26 21:53:53 -0300

    Added front-end product validations

--- public/js/validations/product-validation.js
+++ public/js/validations/product-validation.js
@@ -1,50 +1,86 @@
+// Form fields with any validation requirements
+let productName = document.querySelector('#name');
+let productDescription = document.querySelector('#description');
+let productImage = document.querySelector('#image');
+let productCategory = document.querySelector('#category');
+let productSubcategory = document.querySelector('#subcategory');
+let productDiscount = document.querySelector('#discount');
+let productStarred = document.querySelector('#starred');
+let productPrice = document.querySelector('#price');
+let productStock = document.querySelector('#stock');
 
-let productName = document.querySelector("#name")
-let productDescription = document.querySelector("#description")
-let productImage = document.querySelector("#image")
-let productCategory = document.querySelector("#category")
-let productSubcategory = document.querySelector("#subcategory")
-let productDiscount = document.querySelector("#discount")
-let productStarred = document.querySelector("#starred")
-let productPrice = document.querySelector("#price")
-let productStock = document.querySelector("#stock")
-let productForm = document.querySelector("main form")
+// Prevent submit event & print error messages in HTML
+const productForm = document.querySelector('main form');
+const ulErrors = document.querySelector('.product-validation-errors');
 
+productForm.addEventListener('submit', (event) => {
+  const validationErrors = formIsInvalid();
 
-productForm.addEventListener("submit", (event) => {
+  if (validationErrors.length > 0) {
     event.preventDefault();
 
-    let errors = []
+    ulErrors.classList.remove('hidden');
+    ulErrors.innerHTML = '';
 
-    if(productName.value.length < 5){
-        // AGREGAR VALIDACIONES
-        errors.push("El nombre debe tener al menos 5 caracteres")
-        console.log("nombre menos a 5")
+    for (const error of validationErrors) {
+      ulErrors.innerHTML += `<li> ${error} </li>`;
     }
-    if(productName.value.trim() == ""){
-        errors.push("Debes ingresar un nombre")
+  } else {
+    ulErrors.classList.add('hidden');
+    ulErrors.innerHTML = '';
+  }
+});
 
-        // AGREGAR VALIDACIONES
+function formIsInvalid() {
+  let errors = [];
+
+  if (productName.value.trim() == '') {
+    errors.push('Debes ingresar un nombre');
+    productName.classList.add('is-invalid');
+  } else {
+    productName.classList.remove('is-invalid');
+    productName.classList.add('is-valid');
+  }
+
+  if (productName.value.length < 5) {
+    productName.classList.add('is-invalid');
+    errors.push('El campo de nombre debe tener al menos 5 caracteres');
+  } else {
+    productName.classList.remove('is-invalid');
+    productName.classList.add('is-valid');
+  }
+
+  if (productDescription.value.length < 20) {
+    productDescription.classList.add('is-invalid');
+    errors.push('La descripcion debe ser de 20 caracteres o mas');
+  } else {
+    productDescription.classList.remove('is-invalid');
+    productDescription.classList.add('is-valid');
+  }
+
+  return errors;
+}
+
+/* PASOS A SEGUIR:
+
+0. Asignar 1 clase a todos los inputs que quiero validar y almacenarlos en 1 variable.
+1. Con un FOR EACH preguntar si el input seleccionado es el que quiero validar.
+2. Escribir la condicion y agregar las clases necesarias.
+3.
+
+  const fields = document.querySelectorAll('.fields');
+
+  fields.forEach((field) => {
+    if (field.name == 'name' && field.value.trim() == '') {
+      Aca ponemos las condiciones y las clases bla bla bla
     }
-    if(productDescription.value.length < 20){
-        // AGREGAR VALIDACIONES
-        errors.push("La descripcion debe ser de 20 caracteres o mas")
 
-        
-    }
-    //Validacion imagenes
-
-    //Validacion final
-    console.log(errors)
-    if (errors.length > 0) {
-        let ulErrors = document.querySelector('.product-validation-errors');
-        for (let i = 0; i < errors.length; i++) {
-            ulErrors.innerHTML += `<li >  ${errors[i]} </li>`;
-        };
-    }else{
-        alert('La validación fué exitosa')
-        form.submit();
+    if (field.name == 'name' && field.value.length < 5) {
+      Aca ponemos las condiciones y las clases bla bla bla
     }
 
-})
+    Etc etc etc
 
+  });
+
+*/
--- src/routes/admin-routes.js
+++ src/routes/admin-routes.js
@@ -1,38 +1,38 @@
 // ***** Requires *****
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const path = require("path");
-const multer = require("multer");
+const path = require('path');
+const multer = require('multer');
 
 // ***** Controller Require *****
-const adminController = require("../controllers/admin-controller-DB");
+const adminController = require('../controllers/admin-controller-DB');
 
 // -------- AUTH MIDDLEWARE -----------
-let LoginMiddlewares = require("../middlewares/loginMiddlewares");
-let productValidation = require('../validations/products-validations')
+let loginMiddlewares = require('../middlewares/loginMiddlewares');
+let productValidation = require('../validations/products-validations');
 
 // ***** Initializing MULTER *****
 const storage = multer.diskStorage({
-  destination: path.join(__dirname, "../../public/img/products"),
+  destination: path.join(__dirname, '../../public/img/products'),
   filename: (req, file, cb) => {
     // Validar el tamaño del archivo
     //if(file.size > 10 * 1000 * 1000){
     //    cb(new Error("Archivo demasiado grande"))}
-    cb(null, Date.now() + "-" + file.originalname);
+    cb(null, Date.now() + '-' + file.originalname);
   },
 });
 
 const uploader = multer({ storage });
 
 // ----- CREATE ONE PRODUCT -----
-router.get("/product/create", /**LoginMiddlewares.authMiddleware,  */ adminController.create);
-router.post("/product/create", uploader.single("image"),/** productValidation, */adminController.store);
+router.get('/product/create', loginMiddlewares.authMiddleware, adminController.create);
+router.post('/product/create', uploader.single('image'), productValidation, adminController.store);
 
 // ----- EDIT ONE PRODUCT -----
-router.get("/product/:id/edit", LoginMiddlewares.authMiddleware, adminController.edit);
-router.put("/product/:id", uploader.single("image"), adminController.update);
+router.get('/product/:id/edit', loginMiddlewares.authMiddleware, adminController.edit);
+router.put('/product/:id', uploader.single('image'), adminController.update);
 
 // ----- DELETE ONE PRODUCT -----
-router.delete("/product/:id", LoginMiddlewares.authMiddleware, adminController.destroy);
+router.delete('/product/:id', loginMiddlewares.authMiddleware, adminController.destroy);
 
 module.exports = router;
